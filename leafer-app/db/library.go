f89c1fe83f6668924ee783ddb784cc9c06f172eb
Benjamin Petetot <bpetetot@gmail.com>
2020-05-21 14:08:13 +0200

    :recycle: locate model with db functions

--- leafer-app/db/library.go
+++ leafer-app/db/library.go
@@ -1,8 +1,20 @@
 package db
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 )
+
+// Library model
+type Library struct {
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Name      string    `json:"name"`
+	Path      string    `json:"path"`
+	Medias    *[]Media  `json:"medias,omitempty"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
 
 // DeleteLibrary delete the library and its medias
 func DeleteLibrary(library *Library, conn *gorm.DB) {
--- leafer-app/db/media.go
+++ leafer-app/db/media.go
@@ -1,18 +1,6 @@
 package db
 
-import (
-	"time"
-)
-
-// Library model
-type Library struct {
-	ID        uint      `json:"id" gorm:"primary_key"`
-	Name      string    `json:"name"`
-	Path      string    `json:"path"`
-	Medias    *[]Media  `json:"medias,omitempty"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-}
+import "time"
 
 // Media model
 type Media struct {
